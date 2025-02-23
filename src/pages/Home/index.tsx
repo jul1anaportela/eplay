1d760d5da4d9f667692cfe3d0f92614fdf51bf35
jul1anaportela <juliana-portela@outlook.com>
2025-02-23 18:08:29 -0300

    criação de rotas

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,96 @@
+import Banner from '../../components/Banner'
+import ProductsList from '../../components/ProductsList'
+import Game from '../../models/Game'
+
+import residentEvil from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import starWars from '../../assets/images/star_wars.png'
+import zelda from '../../assets/images/zelda.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description: '...',
+    title: 'Resident Evil',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: residentEvil
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description: '...',
+    title: 'Resident Evil',
+    system: 'PS5',
+    infos: ['5%', 'R$ 290,00'],
+    image: residentEvil
+  },
+  {
+    id: 3,
+    category: 'Ação',
+    description: '...',
+    title: 'Resident Evil',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: residentEvil
+  },
+  {
+    id: 4,
+    category: 'Ação',
+    description: '...',
+    title: 'Resident Evil',
+    system: 'Windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: residentEvil
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'RPG',
+    description: '...',
+    title: 'Diablo 4',
+    system: 'Windows',
+    infos: ['17/04'],
+    image: diablo
+  },
+  {
+    id: 6,
+    category: 'RPG',
+    description: '...',
+    title: 'Zelda',
+    system: 'Windows',
+    infos: ['17/04'],
+    image: zelda
+  },
+  {
+    id: 7,
+    category: 'RPG',
+    description: '...',
+    title: 'Star Wars',
+    system: 'Windows',
+    infos: ['17/04'],
+    image: starWars
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description: '...',
+    title: 'Resident Evil',
+    system: 'Nintendo Switch',
+    infos: ['17/04'],
+    image: residentEvil
+  }
+]
+
+export const Home = () => (
+  <>
+    <Banner />
+    <ProductsList games={promocoes} title="Promoções" background="gray" />
+    <ProductsList games={emBreve} title="Em Breve" background="black" />
+  </>
+)
+
+export default Home
